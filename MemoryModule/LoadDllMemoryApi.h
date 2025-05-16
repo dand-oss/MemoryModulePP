5468e952a123fdd366aa09e807b293ab11baa93a
Dan Dees <dand@appsmiths.com>
2025-05-16 13:31:41 +0700

    BUG - LoadDllMemoryApi.h - do not declare NTAPI functions extern "C" def file export list expects no extern "C"

--- MemoryModule/LoadDllMemoryApi.h
+++ MemoryModule/LoadDllMemoryApi.h
@@ -11,29 +11,25 @@ typedef HMODULE HMEMORYMODULE;
 #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
 #endif
 
-extern "C" {
+HMEMORYMODULE WINAPI LoadLibraryMemory(_In_ PVOID BufferAddress);
 
-	HMEMORYMODULE WINAPI LoadLibraryMemory(_In_ PVOID BufferAddress);
+HMEMORYMODULE WINAPI LoadLibraryMemoryExA(
+	_In_ PVOID BufferAddress,
+	_In_ size_t Reserved,
+	_In_opt_ LPCSTR DllBaseName,
+	_In_opt_ LPCSTR DllFullName,
+	_In_ DWORD Flags
+);
 
-	HMEMORYMODULE WINAPI LoadLibraryMemoryExA(
-		_In_ PVOID BufferAddress,
-		_In_ size_t Reserved,
-		_In_opt_ LPCSTR DllBaseName,
-		_In_opt_ LPCSTR DllFullName,
-		_In_ DWORD Flags
-	);
+HMEMORYMODULE WINAPI LoadLibraryMemoryExW(
+	_In_ PVOID BufferAddress,
+	_In_ size_t Reserved,
+	_In_opt_ LPCWSTR DllBaseName,
+	_In_opt_ LPCWSTR DllFullName,
+	_In_ DWORD Flags
+);
 
-	HMEMORYMODULE WINAPI LoadLibraryMemoryExW(
-		_In_ PVOID BufferAddress,
-		_In_ size_t Reserved,
-		_In_opt_ LPCWSTR DllBaseName,
-		_In_opt_ LPCWSTR DllFullName,
-		_In_ DWORD Flags
-	);
-
-	BOOL WINAPI FreeLibraryMemory(_In_ HMEMORYMODULE hMemoryModule);
-
-}
+BOOL WINAPI FreeLibraryMemory(_In_ HMEMORYMODULE hMemoryModule);
 
 #define NtLoadDllMemory						LdrLoadDllMemory
 #define NtLoadDllMemoryExA					LdrLoadDllMemoryExA
