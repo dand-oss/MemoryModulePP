22d4625086295eaffc2c416d56fddc75c5f9f340
Dan Dees <dand@appsmiths.com>
2025-05-17 21:26:22 +0700

    prefer C++ style casting     - safer       - overrides fewer attributes seen by compiler     - more explicit       - shows where more serious casing done       - invites templates/inlines to minimize

--- MemoryModule/MemoryModule.cpp
+++ MemoryModule/MemoryModule.cpp
@@ -12,9 +12,9 @@
 
 #define GET_HEADER_DICTIONARY(headers, idx)  &headers->OptionalHeader.DataDirectory[idx]
 
-#define AlignValueUp(value, alignment) ((size_t(value) + size_t(alignment) - 1) & ~(size_t(alignment) - 1))
+#define AlignValueUp(value, alignment) ((static_cast<size_t>(value) + static_cast<size_t>(alignment) - 1) & ~(static_cast<size_t>(alignment) - 1))
 
-#define OffsetPointer(data, offset) LPVOID(LPBYTE(data) + ptrdiff_t(offset))
+#define OffsetPointer(data, offset) reinterpret_cast<LPVOID>(reinterpret_cast<LPBYTE>(data) + ptrdiff_t(offset))
 
 // Protection flags for memory pages (Executable, Readable, Writeable)
 static const int ProtectionFlags[2][2][2] = {
@@ -30,8 +30,8 @@ static const int ProtectionFlags[2][2][2] = {
 };
 
 int MmpSizeOfImageHeadersUnsafe(PVOID BaseAddress) {
-	PIMAGE_DOS_HEADER dh = (PIMAGE_DOS_HEADER)BaseAddress;
-	PIMAGE_NT_HEADERS nh = (PIMAGE_NT_HEADERS)((LPBYTE)BaseAddress + dh->e_lfanew);
+	PIMAGE_DOS_HEADER dh = static_cast<PIMAGE_DOS_HEADER>(BaseAddress);
+	PIMAGE_NT_HEADERS nh = reinterpret_cast<PIMAGE_NT_HEADERS>(static_cast<LPBYTE>(BaseAddress) + dh->e_lfanew);
 
 	//https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_optional_header32
 	int sizeOfHeaders = dh->e_lfanew +										// e_lfanew member of IMAGE_DOS_HEADER
@@ -50,8 +50,8 @@ PMEMORYMODULE WINAPI MapMemoryModuleHandle(HMEMORYMODULE hModule) {
 	if (!nh)return nullptr;
 
 	int sizeOfHeaders = MmpSizeOfImageHeadersUnsafe(hModule);
-	PMEMORYMODULE pModule = (PMEMORYMODULE)((LPBYTE)hModule + sizeOfHeaders);
-	if (pModule->Signature != MEMORY_MODULE_SIGNATURE || pModule->codeBase != (LPBYTE)hModule)return nullptr;
+	PMEMORYMODULE pModule = reinterpret_cast<PMEMORYMODULE>((LPBYTE)hModule + sizeOfHeaders);
+	if (pModule->Signature != MEMORY_MODULE_SIGNATURE || pModule->codeBase != reinterpret_cast<LPBYTE>(hModule))return nullptr;
 	return pModule;
 }
 
@@ -66,7 +66,7 @@ NTSTATUS MmpInitializeStructure(DWORD ImageFileSize, LPCVOID ImageFileBuffer, PI
 	//
 	// Make sure there have enough free space to embed our structure.
 	//
-	int sizeOfHeaders = MmpSizeOfImageHeadersUnsafe((PVOID)ImageHeaders->OptionalHeader.ImageBase);
+	int sizeOfHeaders = MmpSizeOfImageHeadersUnsafe(reinterpret_cast<PVOID>(ImageHeaders->OptionalHeader.ImageBase));
 	PIMAGE_SECTION_HEADER pSections = IMAGE_FIRST_SECTION(ImageHeaders);
 	for (int i = 0; i < ImageHeaders->FileHeader.NumberOfSections; ++i) {
 		if (pSections[i].VirtualAddress < sizeOfHeaders + sizeof(MEMORYMODULE)) {
@@ -77,13 +77,13 @@ NTSTATUS MmpInitializeStructure(DWORD ImageFileSize, LPCVOID ImageFileBuffer, PI
 	//
 	// Setup MemoryModule structure.
 	//
-	PMEMORYMODULE hMemoryModule = (PMEMORYMODULE)(ImageHeaders->OptionalHeader.ImageBase + sizeOfHeaders);
+	PMEMORYMODULE hMemoryModule = reinterpret_cast<PMEMORYMODULE>(ImageHeaders->OptionalHeader.ImageBase + sizeOfHeaders);
 	RtlZeroMemory(hMemoryModule, sizeof(MEMORYMODULE));
-	hMemoryModule->codeBase = (PBYTE)ImageHeaders->OptionalHeader.ImageBase;
+	hMemoryModule->codeBase = reinterpret_cast<PBYTE>(ImageHeaders->OptionalHeader.ImageBase);
 	hMemoryModule->dwImageFileSize = ImageFileSize;
 	hMemoryModule->Signature = MEMORY_MODULE_SIGNATURE;
 	hMemoryModule->SizeofHeaders = ImageHeaders->OptionalHeader.SizeOfHeaders;
-	hMemoryModule->lpReserved = (LPVOID)ImageFileBuffer;
+	hMemoryModule->lpReserved = const_cast<LPVOID>(ImageFileBuffer);
 	hMemoryModule->dwReferenceCount = 1;
 
 	return STATUS_SUCCESS;
@@ -96,7 +96,7 @@ NTSTATUS MemorySetSectionProtection(
 	PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(lpNtHeaders);
 
 	for (DWORD i = 0; i < lpNtHeaders->FileHeader.NumberOfSections; ++i, ++section) {
-		LPVOID address = LPBYTE(base) + section->VirtualAddress;
+		LPVOID address = static_cast<LPBYTE>(base) + section->VirtualAddress;
 		SIZE_T size = AlignValueUp(section->Misc.VirtualSize, lpNtHeaders->OptionalHeader.SectionAlignment);
 
 		BOOL executable = (section->Characteristics & IMAGE_SCN_MEM_EXECUTE) != 0,
@@ -133,7 +133,7 @@ NTSTATUS MemoryLoadLibrary(
 		//
 		// Check dos magic
 		//
-		dos_header = (PIMAGE_DOS_HEADER)data;
+		dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(const_cast<LPVOID>(data));
 		if (dos_header->e_magic != IMAGE_DOS_SIGNATURE) {
 			status = STATUS_INVALID_IMAGE_FORMAT;
 			__leave;
@@ -142,7 +142,7 @@ NTSTATUS MemoryLoadLibrary(
 		//
 		// Check nt headers
 		//
-		old_header = (PIMAGE_NT_HEADERS)((size_t)data + dos_header->e_lfanew);
+		old_header = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<size_t>(data) + dos_header->e_lfanew);
 		if (old_header->Signature != IMAGE_NT_SIGNATURE ||
 			old_header->OptionalHeader.SectionAlignment & 1) {
 			status = STATUS_INVALID_IMAGE_FORMAT;
@@ -175,20 +175,20 @@ NTSTATUS MemoryLoadLibrary(
 	//
 	LPBYTE base = nullptr;
 	if ((old_header->OptionalHeader.DllCharacteristics & IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE) == 0) {
-		base = (LPBYTE)VirtualAlloc(
-			LPVOID(old_header->OptionalHeader.ImageBase),
+		base = static_cast<LPBYTE>(VirtualAlloc(
+			reinterpret_cast<LPVOID>(old_header->OptionalHeader.ImageBase),
 			old_header->OptionalHeader.SizeOfImage,
 			MEM_RESERVE,
 			PAGE_EXECUTE_READWRITE
-		);
+		));
 	}
 	if (!base) {
-		base = (LPBYTE)VirtualAlloc(
+		base = static_cast<LPBYTE>(VirtualAlloc(
 			nullptr,
 			old_header->OptionalHeader.SizeOfImage,
 			MEM_RESERVE,
 			PAGE_EXECUTE_READWRITE
-		);
+		));
 		if (!base) status = STATUS_NO_MEMORY;
 	}
 
@@ -199,7 +199,7 @@ NTSTATUS MemoryLoadLibrary(
 	//
 	// Allocate memory for image headers
 	//
-	size_t alignedHeadersSize = (DWORD)AlignValueUp(old_header->OptionalHeader.SizeOfHeaders + sizeof(MEMORYMODULE), MmpGlobalDataPtr->SystemInfo.dwPageSize);
+	size_t alignedHeadersSize = static_cast<DWORD>(AlignValueUp(old_header->OptionalHeader.SizeOfHeaders + sizeof(MEMORYMODULE), MmpGlobalDataPtr->SystemInfo.dwPageSize));
 	if (!VirtualAlloc(base, alignedHeadersSize, MEM_COMMIT, PAGE_READWRITE)) {
 		VirtualFree(base, 0, MEM_RELEASE);
 		status = STATUS_NO_MEMORY;
@@ -209,14 +209,14 @@ NTSTATUS MemoryLoadLibrary(
 	//
 	// Copy headers
 	//
-	PIMAGE_DOS_HEADER new_dos_header = (PIMAGE_DOS_HEADER)base;
-	PIMAGE_NT_HEADERS new_header = (PIMAGE_NT_HEADERS)(base + dos_header->e_lfanew);
+	PIMAGE_DOS_HEADER new_dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(base);
+	PIMAGE_NT_HEADERS new_header = reinterpret_cast<PIMAGE_NT_HEADERS>(base + dos_header->e_lfanew);
 	RtlCopyMemory(
 		new_dos_header,
 		dos_header,
 		old_header->OptionalHeader.SizeOfHeaders
 	);
-	new_header->OptionalHeader.ImageBase = (size_t)base;
+	new_header->OptionalHeader.ImageBase = reinterpret_cast<size_t>(base);
 
 	do {
 		//
@@ -241,7 +241,7 @@ NTSTATUS MemoryLoadLibrary(
 			}
 
 			LPVOID dest = VirtualAlloc(
-				(LPSTR)new_header->OptionalHeader.ImageBase + section->VirtualAddress,
+				reinterpret_cast<LPSTR>(new_header->OptionalHeader.ImageBase) + section->VirtualAddress,
 				size,
 				MEM_COMMIT,
 				PAGE_READWRITE
@@ -254,7 +254,7 @@ NTSTATUS MemoryLoadLibrary(
 			if (section->SizeOfRawData) {
 				RtlCopyMemory(
 					dest,
-					LPBYTE(data) + section->PointerToRawData,
+					static_cast<const BYTE*>(data) + section->PointerToRawData,
 					section->SizeOfRawData
 				);
 			}
@@ -282,16 +282,16 @@ NTSTATUS MemoryLoadLibrary(
 			}IMAGE_BASE_RELOCATION_HEADER, * PIMAGE_BASE_RELOCATION_HEADER;
 
 			PIMAGE_DATA_DIRECTORY dir = GET_HEADER_DICTIONARY(new_header, IMAGE_DIRECTORY_ENTRY_BASERELOC);
-			PIMAGE_BASE_RELOCATION_HEADER relocation = (PIMAGE_BASE_RELOCATION_HEADER)(LPBYTE(base) + dir->VirtualAddress);
+			PIMAGE_BASE_RELOCATION_HEADER relocation = reinterpret_cast<PIMAGE_BASE_RELOCATION_HEADER>(static_cast<LPBYTE>(base) + dir->VirtualAddress);
 
 			if (dir->Size && dir->VirtualAddress) {
-				while ((LPBYTE(relocation) < LPBYTE(base) + dir->VirtualAddress + dir->Size) && relocation->VirtualAddress > 0) {
+				while ((reinterpret_cast<LPBYTE>(relocation) < static_cast<LPBYTE>(base) + dir->VirtualAddress + dir->Size) && relocation->VirtualAddress > 0) {
 					auto relInfo = (_REBASE_INFO*)&relocation->TypeOffset;
 					for (DWORD i = 0; i < relocation->TypeOffsetCount(); ++i, ++relInfo) {
 						switch (relInfo->Type) {
-						case IMAGE_REL_BASED_HIGHLOW: *(DWORD*)(base + relocation->VirtualAddress + relInfo->Offset) += (DWORD)locationDelta; break;
+						case IMAGE_REL_BASED_HIGHLOW: *reinterpret_cast<DWORD*>(base + relocation->VirtualAddress + relInfo->Offset) += static_cast<DWORD>(locationDelta); break;
 #ifdef _WIN64
-						case IMAGE_REL_BASED_DIR64: *(ULONGLONG*)(base + relocation->VirtualAddress + relInfo->Offset) += (ULONGLONG)locationDelta; break;
+						case IMAGE_REL_BASED_DIR64: *reinterpret_cast<ULONGLONG*>(base + relocation->VirtualAddress + relInfo->Offset) += static_cast<ULONGLONG>(locationDelta); break;
 #endif
 						case IMAGE_REL_BASED_ABSOLUTE:
 						default: break;
@@ -300,7 +300,7 @@ NTSTATUS MemoryLoadLibrary(
 
 					// advance to next relocation block
 					//relocation->VirtualAddress += module->headers_align;
-					relocation = decltype(relocation)(OffsetPointer(relocation, relocation->SizeOfBlock));
+					relocation = static_cast<decltype(relocation)>(OffsetPointer(relocation, relocation->SizeOfBlock));
 				}
 			}
 
