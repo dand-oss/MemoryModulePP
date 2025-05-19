8c64f6739feeb1dc3ac6362b3e288db630005077
Dan Dees <dand@appsmiths.com>
2025-05-19 15:43:13 +0700

    MemoryLoadLibraryEx provides callback to for complete loader
    https://github.com/fancycode/MemoryModule/blob/master/MemoryModule.c
    
    HMEMORYMODULE MemoryLoadLibraryEx(const void *data, size_t size,
        CustomAllocFunc allocMemory,
        CustomFreeFunc freeMemory,
        CustomLoadLibraryFunc loadLibrary,
        CustomGetProcAddressFunc getProcAddress,
        CustomFreeLibraryFunc freeLibrary,
        void *userdata)
    
    see how
    headers->OptionalHeader.AddressOfEntryPoint
    
    DLL_PROCESS_ATTACH
    
    is handled in
    
    Utils.cpp
    LdrpCallInitlizers
    
    LdrEntry.cpp
    Loader.cpp
    ReflectiveLoader.c

--- MemoryModule/LoadDllMemoryApi.h
+++ MemoryModule/LoadDllMemoryApi.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <Windows.h>
 
-typedef HMODULE HMEMORYMODULE;
+using HMEMORYMODULE = HMODULE;
+using HCUSTOMMODULE = HMODULE;
+
+// Callback type definitions (required for MemoryLoadLibraryEx)
+typedef void* (WINAPI *CustomAllocFunc)(LPVOID, SIZE_T, DWORD, DWORD, void*);
+typedef BOOL (WINAPI *CustomFreeFunc)(LPVOID, SIZE_T, DWORD, void*);
+typedef HCUSTOMMODULE (WINAPI *CustomLoadLibraryFunc)(LPCSTR, void*);
+typedef FARPROC (WINAPI *CustomGetProcAddressFunc)(HCUSTOMMODULE, LPCSTR, void*);
+typedef void (WINAPI *CustomFreeLibraryFunc)(HCUSTOMMODULE, void*);
+
 #include "Loader.h"
 #include "Initialize.h"
-
-#define MemoryModuleToModule(_hMemoryModule_) (_hMemoryModule_)
 
 #ifndef NT_SUCCESS
 #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
@@ -13,35 +20,47 @@ typedef HMODULE HMEMORYMODULE;
 
 extern "C" {
 
-	HMEMORYMODULE WINAPI LoadLibraryMemory(_In_ PVOID BufferAddress);
+HMEMORYMODULE WINAPI LoadLibraryMemory(
+    _In_ PVOID BufferAddress
+);
 
-	HMEMORYMODULE WINAPI LoadLibraryMemoryExA(
-		_In_ PVOID BufferAddress,
-		_In_ size_t Reserved,
-		_In_opt_ LPCSTR DllBaseName,
-		_In_opt_ LPCSTR DllFullName,
-		_In_ DWORD Flags
-	);
+HMEMORYMODULE WINAPI LoadLibraryMemoryExA(
+    _In_ PVOID BufferAddress,
+    _In_ size_t Reserved,
+    _In_opt_ LPCSTR DllBaseName,
+    _In_opt_ LPCSTR DllFullName,
+    _In_ DWORD Flags
+);
 
-	HMEMORYMODULE WINAPI LoadLibraryMemoryExW(
-		_In_ PVOID BufferAddress,
-		_In_ size_t Reserved,
-		_In_opt_ LPCWSTR DllBaseName,
-		_In_opt_ LPCWSTR DllFullName,
-		_In_ DWORD Flags
-	);
+HMEMORYMODULE WINAPI LoadLibraryMemoryExW(
+    _In_ PVOID BufferAddress,
+    _In_ size_t Reserved,
+    _In_opt_ LPCWSTR DllBaseName,
+    _In_opt_ LPCWSTR DllFullName,
+    _In_ DWORD Flags
+);
 
-	BOOL WINAPI FreeLibraryMemory(_In_ HMEMORYMODULE hMemoryModule);
+BOOL WINAPI FreeLibraryMemory(
+    _In_ HMEMORYMODULE hMemoryModule
+);
 
-}
+HMEMORYMODULE WINAPI MemoryLoadLibraryEx(
+    const void* data,
+    size_t size,
+    CustomAllocFunc allocMemory,
+    CustomFreeFunc freeMemory,
+    CustomLoadLibraryFunc loadLibrary,
+    CustomGetProcAddressFunc getProcAddress,
+    CustomFreeLibraryFunc freeLibrary,
+    void* userdata) ;
 
-#define NtLoadDllMemory						LdrLoadDllMemory
-#define NtLoadDllMemoryExA					LdrLoadDllMemoryExA
-#define NtLoadDllMemoryExW					LdrLoadDllMemoryExW
-#define NtUnloadDllMemory					LdrUnloadDllMemory
-#define NtUnloadDllMemoryAndExitThread		LdrUnloadDllMemoryAndExitThread
-#define FreeLibraryMemoryAndExitThread		LdrUnloadDllMemoryAndExitThread
-#define NtQuerySystemMemoryModuleFeatures	LdrQuerySystemMemoryModuleFeatures
+#define NtLoadDllMemory LdrLoadDllMemory
+#define NtLoadDllMemoryExA LdrLoadDllMemoryExA
+#define NtLoadDllMemoryExW LdrLoadDllMemoryExW
+#define NtUnloadDllMemory LdrUnloadDllMemory
+#define NtUnloadDllMemoryAndExitThread LdrUnloadDllMemoryAndExitThread
+#define FreeLibraryMemoryAndExitThread LdrUnloadDllMemoryAndExitThread
+#define NtQuerySystemMemoryModuleFeatures LdrQuerySystemMemoryModuleFeatures
 
 #ifdef UNICODE
 #define LdrLoadDllMemoryEx LdrLoadDllMemoryExW
@@ -52,5 +71,4 @@ extern "C" {
 #endif
 #define NtLoadDllMemoryEx LdrLoadDllMemoryEx
 
-
-
+}
