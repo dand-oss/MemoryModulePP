2c8b3c4107da357e720f292c4a5490e8ab4e7dff
Dan Dees <dand@appsmiths.com>
2025-05-19 10:11:34 +0700

    sharit.sh

---
+++ sharit.sh
@@ -0,0 +1,30 @@
+
+shar $(git ls-files | grep -v 3rdparty) > memorymodulepp.shar.txt
+
+exit
+
+LLM take context as input
+
+often limited to text
+or binaries which it can convert to text (OCR pdf or parse docx)
+
+by attaching files
+or uploading one big file
+
+A shar is a good way to
+
+1. upload a git repo
+or any set of files
+into LLM context
+
+prompt
+add the files in this shar archive as artifacts and give the number of files added, do  not show any artifacts to me
+
+2. and receive all artifacts in result
+
+prompt
+provide all artifacts as a downloadable shar archive
+
+unshar -c memorymodulepp.shar.txt
+
+Strangely, LLM is very stupid about adding hash codes to ensure integrity don't even try!
